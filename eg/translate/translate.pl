c6010ce4f11292061808e0c71e2acf604383f92d
Takatsugu Shigeta <shigeta@cpan.org>
2012-06-01 02:25:31 +0900

    Added translate API sample. But I didn't get responses successfully for translate API. Because I need to pay usage fees for translate API. I think the fees is a little bit expensive for running this test. seealso: https://developers.google.com/translate/v2/pricing

---
+++ eg/translate/translate.pl
@@ -0,0 +1,30 @@
+## If you want to run translate API, you need to pay usage fees.
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature qw/say/;
+
+use FindBin;
+use Google::API::Client;
+use OAuth2::Client;
+
+use lib 'eg/lib';
+use Sample::Utils qw/get_or_restore_token store_token/;
+
+
+my $service = Google::API::Client->new->build('translate', 'v2');
+
+my $file = "$FindBin::Bin/../client_secrets.json";
+my $auth_driver = OAuth2::Client->new_from_client_secrets($file, $service->{auth_doc});
+my $dat_file = "$FindBin::Bin/token.dat";
+my $access_token = get_or_restore_token($dat_file, $auth_driver);
+
+my $res = $service->translations->list(body => {source => 'en', target => 'fr', q => [qw/flower car/]})->execute;
+use Data::Dumper;
+warn Dumper($res);
+
+store_token($dat_file, $auth_driver);
+
+say 'done';
+__END__
