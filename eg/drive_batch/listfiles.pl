2502c09bf56fbe5a09e7a836e16fad7e4773bbc9
José Manuel Rodríguez D <jose93rd@gmail.com>
2023-03-26 20:46:22 -0700

    Add batch example: eg/drive_batch/listfiles.pl

---
+++ eg/drive_batch/listfiles.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use Google::API::Client;
+use Google::API::OAuth2::Client;
+use FindBin;
+use Data::Dumper;
+
+use lib 'eg/lib';
+use Sample::Utils qw/get_or_restore_token store_token/;
+
+my $code   = shift;
+my $client = Google::API::Client->new;
+my ($service, $batch) = $client->build('drive', 'v3');
+my $auth_driver = Google::API::OAuth2::Client->new_from_client_secrets("$FindBin::Bin/../client_secrets.json", $service->{auth_doc});
+
+my $dat_file     = "$FindBin::Bin/token.dat";
+my $access_token = get_or_restore_token($dat_file, $auth_driver, 'drive.readonly');
+
+my $files_a;
+my $files_b;
+
+# 'q' parameter syntax reference: https://developers.google.com/drive/api/guides/search-files
+# Replace with your own queries
+$service->files->list(body => {q => qq{name = 'One Piece Film Red'}})->batch(sub { $files_a = shift });
+$service->files->list(body => {q => qq{name = 'Upcoming PS4 Games'}})->batch(sub { $files_b = shift });
+$batch->execute({auth_driver => $auth_driver});
+
+print Dumper($files_a, $files_b) . "\n";
+store_token($dat_file, $auth_driver);
+__END__
